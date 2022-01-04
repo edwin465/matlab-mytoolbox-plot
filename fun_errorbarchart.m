e488f1e6457ba338169d455f69f1de5bc7be7f1d
Chi Man Wong <chiman465@gmail.com>
2022-01-04 13:40:50 +0800

    Update fun_errorbarchart.m

--- fun_errorbarchart.m
+++ fun_errorbarchart.m
@@ -2,12 +2,15 @@ function fun_errorbarchart(dat_x,dat_y1,dat_y2,test_pval0,title_str,x_lab_str,y_
 % This function is used to compare two input data in the bar chart and
 % error-bar,the statistical test results are shown at the same time.
 %
-% dat_x: 1 x N
-% dat_y1: M x N
+% dat_x: 1 x N   (N: num. of dim.)
+% dat_y1: M x N  (M: num. of samples)
 % dat_y2: M x N
 % test_pval0: 
-% 1) test_pval0: 1 x N (statistical test results between two samples: dat_y1 and dat_y2
-% 2) test_pval0: 1 (Calculate the paired t-test result (tail: both))
+% 1) Calculate by yourself:
+%    test_pval0: 1 x N (statistical test results between two samples:
+%    dat_y1 and dat_y2)
+% 2) Calculate by this function:
+%    test_pval0: 1 (Calculate the paired t-test result (tail: both))
 %                2 (Calculate the independent t-test result (tail: both))
 %                3 (Calculate the signrank test result (tail: both))
 %
@@ -21,7 +24,14 @@ function fun_errorbarchart(dat_x,dat_y1,dat_y2,test_pval0,title_str,x_lab_str,y_
 %
 % Example
 %
-% 
+% M=10;N=4;dat_x=[1:N];dat_y1=rand(M,N);dat_y2=rand(M,N);
+% title_str='y1 vs y2';
+% x_lab_str='Num. of testings';
+% y_lab_str='Score';
+% legend_str1='y1';legend_str2='y2';
+% test_pval0=1;
+% fun_errorbarchart(dat_x,dat_y1,dat_y2,test_pval0,title_str,x_lab_str,y_lab_str,legend_str1,legend_str2)
+
 
 if nargin<9
     disp('Not enough arguments!');
@@ -40,7 +50,8 @@ dat_sd(2,:)=std(dat_y2)/sqrt(size(dat_y2,1));
 
 font_size=30;
 color_code='rb';
-delta_x=[-0.015 0.015];
+dx=dat_x(2)-dat_x(1);
+delta_x=[-0.15 0.15]*dx;
 [d1,d2]=size(dat_mu);
 
 if size(test_pval0,2)==size(dat_mu,2)
@@ -80,20 +91,26 @@ if d1==2
     end
     for n=1:size(dat_x,2)
         if test_pval(n)<0.001
-            plot(dat_x(n),star_ypos,'h','markersize',10,'MarkerFaceColor','k','MarkerEdgeColor','k');
-        elseif test_pval(n)<0.01
+            plot(dat_x(n)-0.01,star_ypos,'p','markersize',10,'MarkerFaceColor','k','MarkerEdgeColor','k');
             plot(dat_x(n),star_ypos,'p','markersize',10,'MarkerFaceColor','k','MarkerEdgeColor','k');
+            plot(dat_x(n)+0.01,star_ypos,'p','markersize',10,'MarkerFaceColor','k','MarkerEdgeColor','k');
+            
+        elseif test_pval(n)<0.01
+            plot(dat_x(n)-0.01,star_ypos,'p','markersize',10,'MarkerFaceColor','k','MarkerEdgeColor','k');
+            plot(dat_x(n)+0.01,star_ypos,'p','markersize',10,'MarkerFaceColor','k','MarkerEdgeColor','k');
+            
         elseif test_pval(n)<0.05
-            plot(dat_x(n),star_ypos,'*','markersize',10,'MarkerFaceColor','k','MarkerEdgeColor','k');
+            plot(dat_x(n),star_ypos,'p','markersize',10,'MarkerFaceColor','k','MarkerEdgeColor','k');
         else
         end
     end
     legend(legend_str1,legend_str2,'location','southeast');
-    title(title_str);
+    title([title_str]);
     xlabel(x_lab_str);
     ylabel(y_lab_str);
     xticks(dat_x);
-    ylim([0 star_ypos*(1.1)]);
+    xlim([dat_x(1)-(dx/2) dat_x(end)+(dx/2)]);
+    ylim([0 max(dat_mu(:))+1.1*max(dat_sd(:))]);
     set(gca,'fontsize',font_size,'linewidth',2);
 else
     disp('check the dimension of input');
